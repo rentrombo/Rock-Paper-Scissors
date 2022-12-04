01370de48ed33e71cc3dcb235e1d4e828cb41fc8
Rowen Trombo <119641186+rentrombo@users.noreply.github.com>
2022-12-04 11:54:02 -0500

    randomized number

---
+++ apps.js
@@ -0,0 +1,16 @@
+const computerchoice = document.getElementById('computer-choice')
+const userChoiceDisplay = document.getElementById('user-choice')
+const resultDisplay = document.getElementById('result')
+const possibleChoices = document.querySelectorAll('button')
+let userChoice
+
+possibleChoices,forEach(possibleChoice,addEventListener('click', (e) =>{
+ userChoice = e.target.dispatchEvent
+ userChoiceDisplay.innerHTML = userChoice
+ generateComputerChoice()
+}))
+
+function generateComputerChoice(){
+    const randomNumber = MAth.floor(Math.random() * possibleChoices.length)  // or use 3 since length is 3
+    console.log(randomNumber)
+}
