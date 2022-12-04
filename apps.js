be2e63b891c8cb4b70a4bbc615ca5da413825061
Rowen Trombo <119641186+rentrombo@users.noreply.github.com>
2022-12-04 12:29:49 -0500

    rt
    Signed-off-by: Rowen Trombo <119641186+rentrombo@users.noreply.github.com>

--- apps.js
+++ apps.js
@@ -4,6 +4,7 @@ const resultDisplay = document.getElementById('result')
 const possibleChoices = document.querySelectorAll('button')
 let userChoice
 let computerChoice
+let result
 
 possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) =>{
  userChoice = e.target.id
@@ -25,4 +26,28 @@ function generateComputerChoice(){
         computerChoice = 'paper'
     }
     computerChoiceDisplay.innerHTML = computerChoice
+}
+
+function getResult(){
+    if(computerChoice === userChoice){
+        result = "It's a tie! Try again!"
+    }
+    if(computerChoice === 'rock' && userChoice === "scissors"){
+        result = "You lose!"
+    }
+    if(computerChoice === 'rock' && userChoice === "paper"){
+        result = "You win!"
+    }
+    if(computerChoice === 'paper' && userChoice === "rock"){
+        result = 'You lose!'
+    }
+    if(computerChoice === 'paper' && userChoice === "scissors"){
+        result = "You win!"
+    }
+    if(computerChoice === 'scissors' && userChoice === "paper"){
+        result = "You lose!"
+    }
+    if(computerChoice === 'scissors' && userChoice === "rock"){
+        result = "You win"
+    }
 }
